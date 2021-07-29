e19ea183a9966a0be6b38e06137f22af44aa6b61
0poss <67114640+0poss@users.noreply.github.com>
2021-07-29 14:45:01 +0000

    Modified & added stuff.

--- Z3Matrix.mli
+++ Z3Matrix.mli
@@ -12,10 +12,10 @@ sig
   val mk_eq : Z3.context -> t -> t -> Z3.Expr.expr
 
   (** Init. a matrix with a function taking the i and j indexes as argument. *)
-  val init : nrows:int -> ncols:int -> f:(int -> int -> Z3.Expr.expr) -> t
+  val mk_matrix : nrows:int -> ncols:int -> f:(int -> int -> Z3.Expr.expr) -> t
 
-  (** Init. a vector with an expression. *)
-  val init' : nrows:int -> Z3.Expr.expr -> t
+  (** Init. a matrix with an expression. *)
+  val mk_matrix' : nrows:int -> ncols:int -> Z3.Expr.expr -> t
 
   val map : t -> f:(Z3.Expr.expr -> Z3.Expr.expr) -> t
 end
