0c5bd10f09db4566ef15df406c928d15397637c7
0poss <brnnrlxndr@gmail.com>
2021-07-30 10:30:39 +0000

    Added mk_matrix_arr the function.

--- Z3Matrix.mli
+++ Z3Matrix.mli
@@ -14,10 +14,15 @@ sig
   (** Init. a matrix with a function taking the i and j indexes as argument. *)
   val mk_matrix : nrows:int -> ncols:int -> f:(int -> int -> Z3.Expr.expr) -> t
 
-  (** Init. a matrix with an expression. *)
-  val mk_matrix' : nrows:int -> ncols:int -> Z3.Expr.expr -> t
+  (** Init. a matrix with an array. *)
+  val mk_matrix_arr : Z3.Expr.expr array array -> t
 
+  (** Init. a matrix with an expression. *)
+  val mk_matrix_const : nrows:int -> ncols:int -> Z3.Expr.expr -> t
+
+  (** A map function. *)
   val map : t -> f:(Z3.Expr.expr -> Z3.Expr.expr) -> t
 
+  (** An iter function. *)
   val iter : t -> f:(Z3.Expr.expr -> unit) -> unit
 end
